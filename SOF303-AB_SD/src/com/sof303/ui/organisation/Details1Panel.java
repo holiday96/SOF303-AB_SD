a9117ecda1e2826550d7af703ade7128b1fe61f5
iamducthanh <ducthanh260801@gmail.com>
2021-05-26 18:57:07 +0700

    hú

---
+++ SOF303-AB_SD/src/com/sof303/ui/organisation/Details1Panel.java
@@ -0,0 +1,14 @@
+package com.sof303.ui.organisation;
+
+import javax.swing.JPanel;
+
+public class Details1Panel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Details1Panel() {
+
+	}
+
+}
---
+++ SOF303-AB_SD/src/com/sof303/ui/organisation/Details2Panel.java
@@ -0,0 +1,14 @@
+package com.sof303.ui.organisation;
+
+import javax.swing.JPanel;
+
+public class Details2Panel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Details2Panel() {
+
+	}
+
+}
