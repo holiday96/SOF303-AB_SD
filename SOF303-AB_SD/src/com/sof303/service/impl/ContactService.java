c47d0a2c8eace5181a1fa6a3119b3d8b29865809
quang <quanngoc201197@gmail.com>
2021-05-27 21:49:35 +0700

    load table service list

--- SOF303-AB_SD/src/com/sof303/service/impl/ContactService.java
+++ SOF303-AB_SD/src/com/sof303/service/impl/ContactService.java
@@ -1,6 +1,28 @@
 package com.sof303.service.impl;
 
+import com.sof303.dao.IContactDAO;
+import com.sof303.dao.impl.ContactDAO;
+import com.sof303.model.ContactModel;
 import com.sof303.service.IContactService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ContactService implements IContactService {
+
+    private static IContactDAO iContactDAO = new ContactDAO();
+    private static List<ContactModel> lstContactModels = iContactDAO.findAll();
+
+    @Override
+    public List<ContactModel> findAll() {
+        return lstContactModels;
+    }
+
+    @Override
+    public String getNameById(int id) {
+        for (ContactModel e : lstContactModels) {
+            if (e.getId().equals(id)) return e.getFirstName();
+        }
+        return null;
+    }
 }
